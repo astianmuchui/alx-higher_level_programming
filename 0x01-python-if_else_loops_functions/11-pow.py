d032906c4a8d98496632bea5a988a004fb37b45c
astianmuchui <astianmuchui@gmail.com>
2023-08-10 17:07:51 +0300

    exponent

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,15 +2,5 @@
 
 def pow(a, b):
 
-    res = 1
-
-    if b == 0:
-        res = 1
-
-    elif b == 1:
-        res = a
-
-    else:
-        for i in range(b + 1):
-            res *= a
+    res = a ** b
     return res
