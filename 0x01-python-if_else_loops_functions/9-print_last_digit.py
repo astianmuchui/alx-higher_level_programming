3a35fc8c91b0911080a8bc18fd8b427edc56f39f
astianmuchui <astianmuchui@gmail.com>
2023-08-10 16:44:01 +0300

    added edge case

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,8 @@ def print_last_digit(number):
 
     if isinstance(number, int):
         last_dig = abs(number) % 10
+
+    if number == 0:
+        last_dig = 0
+
     print(last_dig)
-    return last_dig
