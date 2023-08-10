f614a89fe079abaafffc503a85a9e3cca34dcfc1
astianmuchui <astianmuchui@gmail.com>
2023-08-10 14:23:46 +0300

    last digit tast

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+dig = abs(number) % 10
+
+if dig > 5:
+    print(f"Last digit of {number} is {dig} and is greater than 5")
+elif dig == 0:
+    print(f"Last digit of {number} is {dig} and is 0")
+elif dig != 0 and dig < 6:
+    print(f"Last digit of {number} is {dig} and is less than 6 and not 0")
