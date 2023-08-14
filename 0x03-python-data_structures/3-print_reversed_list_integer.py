acbc826875048b1f98e72ef6da1aa7f5d9db0b58
astianmuchui <astianmuchui@gmail.com>
2023-08-14 06:10:54 +0300

    print reversed list of integers

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    max_idx = len(my_list) - 1
+    for n in my_list:
+        print("{:d}".format(my_list[max_idx]))
+        max_idx -= 1
