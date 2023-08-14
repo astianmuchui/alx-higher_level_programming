75836cb89f6872464089de25cf3db9dc6b5556ea
astianmuchui <astianmuchui@gmail.com>
2023-08-14 06:20:05 +0300

    listcpy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list
+    if idx > 0 and idx < len(my_list):
+        x = my_list[idx]
+        list_cpy[idx] = element
+        return list_cpy
+    else:
+        return my_list
