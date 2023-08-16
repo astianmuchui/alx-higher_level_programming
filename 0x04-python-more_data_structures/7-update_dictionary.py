312ee1356916413b4609dfc9e2a54545952587d1
astianmuchui <astianmuchui@gmail.com>
2023-08-16 12:34:07 +0300

    update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-
-
 def update_dictionary(a_dictionary, key, value):
-
     if isinstance(key, str):
-        if a_dictionary.get(key):
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
-    else:
-        return
+        a_dictionary[key] = value
+        return a_dictionary
