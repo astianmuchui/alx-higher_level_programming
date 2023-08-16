92bf4f143b8db6564b4bf2dad12c96064cdb8ff4
astianmuchui <astianmuchui@gmail.com>
2023-08-16 12:15:54 +0300

    rm trailing whitespace

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,5 @@
 
 def only_diff_elements(set_1, set_2):
     new = [x for x in set_1 if x in set_1 and x not in set_2]
-    new_2 = [x for x in set_2 if x in set_2 and x not in set_1]    
+    new_2 = [x for x in set_2 if x in set_2 and x not in set_1]
     return set(new + new_2)
