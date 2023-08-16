2248e7e3c46ee81d7f41f5d63f9d755e88de2fb2
astianmuchui <astianmuchui@gmail.com>
2023-08-16 13:37:53 +0300

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string or isinstance(roman_string, str) is False:
+        return None
+
+    param_t = [x.upper() for x in roman_string]
+
+    dict_t = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    keys = dict_t.keys
+    sum = 0
+    for i in param_t:
+        if dict_t.get(i):
+            sum += dict_t.get(i)
+        else:
+            return None
+    return sum
