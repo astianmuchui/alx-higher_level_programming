f41336291ea0403daa45d4240c9ffaa4c5a72c8f
astianmuchui <astianmuchui@gmail.com>
2023-08-16 13:04:09 +0300

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if isinstance(a_dictionary, dict):
-        return max(a_dictionary, key=a_dictionary.get)
+        if isinstance(max(a_dictionary, key=a_dictionary.get), int):
+            return max(a_dictionary, key=a_dictionary.get)
+        else:
+            return None
     else:
         return None
