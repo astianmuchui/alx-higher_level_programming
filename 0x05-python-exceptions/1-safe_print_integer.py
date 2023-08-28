072ebe9c026f7cc9dbc55da54c7af38915d816f8
astianmuchui <astianmuchui@gmail.com>
2023-08-28 12:05:12 +0300

    Almost done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except Exception:
         return False
 
     finally:
