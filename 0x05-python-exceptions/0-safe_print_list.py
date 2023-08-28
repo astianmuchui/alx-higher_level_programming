f2fc9058e6eb4f150ad2b2f29517bf51e5c865de
astianmuchui <astianmuchui@gmail.com>
2023-08-28 12:18:13 +0300

    fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         val = 0
-        for i in range(x + 1):
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
             val += 1
         print()
