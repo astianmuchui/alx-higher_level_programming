1368cc8e6c50fc890d9db161db46c1a48a83699f
astianmuchui <astianmuchui@gmail.com>
2023-08-28 10:31:51 +0300

    print stderr messages

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=stderr)
+        return False
