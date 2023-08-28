2f4b254581d3e9ef765317feb9ccbd791fe4ef02
astianmuchui <astianmuchui@gmail.com>
2023-08-28 10:39:44 +0300

    safely execute python functions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+
+    try:
+        return fct(*args)
+
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=stderr)
+        return None
