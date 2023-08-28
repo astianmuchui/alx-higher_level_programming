112975478553b36a55950919dc2e3405a93019a0
astianmuchui <astianmuchui@gmail.com>
2023-08-28 09:28:13 +0300

    safe print list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    j = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                j += 1
+            else:
+                pass
+    except IndexError:
+        pass
+
+    except TypeError:
+        pass
+
+    finally:
+        print()
+        return (j)
