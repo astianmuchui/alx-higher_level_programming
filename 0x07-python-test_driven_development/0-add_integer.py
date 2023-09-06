5e3559d649a2122d7ccffc0db4e385afb20b5fa8
astianmuchui <astianmuchui@gmail.com>
2023-09-06 10:03:09 +0300

    integer addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Addition of two integers, with one predefined
+"""
+
+
+def add_integer(a, b=98):
+
+    '''
+    Addition of two integers, with one predefined (98)
+    An exception is raised if a or b are not integers or
+    floats
+    '''
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
