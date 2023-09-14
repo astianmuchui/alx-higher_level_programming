7a6c52e95810dab73b0f16fd3658463cae0efa4d
astianmuchui <astianmuchui@gmail.com>
2023-09-14 15:29:51 +0300

    pycodestyle validation and permission handling

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+'''
+    Returns the list of available attributes and methods in an obj
+'''
+
+
 def lookup(obj):
     '''
     Returns the list of available attributes and methods in an obj
     '''
-    return dir(obj)
+    return (dir(obj))
