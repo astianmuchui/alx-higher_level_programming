de475cd95ead8b236a501943b4db93e1db657281
astianmuchui <astianmuchui@gmail.com>
2023-10-01 11:51:58 +0300

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+'''class MyList that inherits from list obj'''
+class MyList(list):
+    """class MyList that inherits from list obj"""
+    def print_sorted(self):
+        """prints the list, but in sorted (ascending sort) format """
+        print(sorted(self))
+
