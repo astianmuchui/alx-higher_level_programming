8b6d58d8324dc2c6f164c90a9cf7a8bca26ba640
astianmuchui <astianmuchui@gmail.com>
2023-10-01 14:49:02 +0300

    module documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-
 """
 add item
 """
+
+
+from sys import argv
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
