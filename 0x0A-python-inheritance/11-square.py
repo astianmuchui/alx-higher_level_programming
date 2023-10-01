424487b9dbd69b08522384c02fccb3366f3be2bd
astianmuchui <astianmuchui@gmail.com>
2023-10-01 13:28:22 +0300

    square 2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Create a subclass of Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Instantiation with size private inst attr"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+
+        return self.__size ** 2
+
+    def __str__(self):
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
