55a99992bb057f87082339d6bbbadee0837f41e2
astianmuchui <astianmuchui@gmail.com>
2023-10-01 14:46:25 +0300

    Rearranged module documentation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import json
 
 """
 A module to write an Obj to a file, using a JSON notation
 """
+
+
+from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
@@ -11,4 +13,4 @@ def save_to_json_file(my_obj, filename):
      A function to write an Obj to a file, using a JSON notation
     """
     with open(filename, encoding="utf-8", mode="w") as file:
-        file.write(json.dumps(my_obj))
+        file.write(dumps(my_obj))
