2c22ec55404a26a45733639d7e181e8b54300544
astianmuchui <astianmuchui@gmail.com>
2023-10-17 16:32:43 +0300

    more tasks

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Gets the top score
+
+SELECT `score`, `name` FROM `second_table` WHERE `score` >=10  ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Creates a table if it does not exist
+-- Adds values
+
+CREATE TABLE IF NOT EXISTS `second_table`(
+        id INT,
+        name VARCHAR(256),
+        score INT
+);
+
+INSERT INTO `cart` (`id`, `name`, `score`, `created_on`) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 18);
