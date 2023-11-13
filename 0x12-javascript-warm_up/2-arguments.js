2c92f3881cdb5bbd664039bdbad2b63785c36abc
astianmuchui <astianmuchui@gmail.com>
2023-11-13 13:36:55 +0300

    commandline arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument Found');
+} else {
+  console.log(`Arguments  ${args}`);
+}
