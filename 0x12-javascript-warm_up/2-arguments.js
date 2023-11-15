d55e3cbb5900be199623c8d3746c79053fcbfbbf
astianmuchui <astianmuchui@gmail.com>
2023-11-15 04:48:55 +0300

    fix bug

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv.length - 2;
 if (args === 0) {
   console.log('No argument');
 } else if (args === 1) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments Found');
+  console.log('Arguments found');
 }
