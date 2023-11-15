fa29ee2c1f0ea2f115ab8c1de459bd1b6dbbf1ce
astianmuchui <astianmuchui@gmail.com>
2023-11-15 04:50:30 +0300

    fix bug

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const process = require('process');
 const arg = process.argv;
 
 if (parseInt(arg[2])) {
-  console.log('My Number: ' + parseInt(arg[2]));
+  console.log('My number: ' + parseInt(arg[2]));
 } else {
   console.log('Not a number');
 }
