534454faeb3ab308235de86cb4ac9979e09c5050
astianmuchui <astianmuchui@gmail.com>
2023-11-15 04:51:39 +0300

    fix bug

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const tools = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const tools = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const tool of tools) {
   console.log(tool);
