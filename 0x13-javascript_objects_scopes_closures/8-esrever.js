5a79d44f83df17073e185166f6fc321488d048d9
astianmuchui <astianmuchui@gmail.com>
2023-11-15 03:58:08 +0300

    reverse a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.erserver = function (list) {
+exports.esrever = function (list) {
   const newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
     newList.push(list[i]);
