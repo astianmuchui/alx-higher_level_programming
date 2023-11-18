86c6e606ead14bf3daa0f7d5dae690a43b5cc903
astianmuchui <astianmuchui@gmail.com>
2023-11-18 14:52:14 +0300

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,14 +10,7 @@ if __name__ == "__main__":
                        passwd=argv[2], db=argv[3], port=3306)
 
     cursor = conn.cursor()
-    cursor.execute(
-        """
-        SELECT `cities`.id, `cities`.`name,` `states`.`name`
-        FROM `cities`
-        JOIN `states`
-        ON `states`.`id` = `cities`.`state_id`
-        ORDER BY `cities`.id"""
-        )
+    cursor.execute("SELECT * FROM `cities` ORDER BY `cities`.`id` ASC")
 
     for row in cursor.fetchall():
         print(row)
