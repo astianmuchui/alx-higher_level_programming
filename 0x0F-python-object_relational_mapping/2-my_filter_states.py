cee079b586c8b7bd98d0740cf59a6048a8f91820
astianmuchui <astianmuchui@gmail.com>
2023-11-18 13:43:19 +0300

    fixed sql query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
                        passwd=argv[2], db=argv[3], port=3306)
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE name\
-                   LIKE %" + argv[4] + "% ORDER BY `states`.`id` ASC")
-
+    cursor.prepare("SELECT * FROM `states` WHERE name\
+                   LIKE %s ORDER BY `states`.`id` ASC")
+    cursor.execute(cursor.statement, (argv[4],))
     for row in cursor.fetchall():
         print(row)
 
