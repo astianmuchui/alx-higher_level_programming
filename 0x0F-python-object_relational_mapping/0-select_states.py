6cfec9d7291f92fcfb7c17d0ddb22dcc7dd30f5f
astianmuchui <astianmuchui@gmail.com>
2023-11-18 11:01:03 +0300

    attempted to fix task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                        passwd=argv[3], db=argv[4], port=3306)
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM `states` ORDER BY `states`.`id` ASC")
 
     for row in cursor.fetchall():
         print(row)
