c9efe4747211899a4e02c6ba201bd639c5d5fea3
astianmuchui <astianmuchui@gmail.com>
2023-11-18 13:08:28 +0300

    attempted task 3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Module to list all states from the database hbtn_0e_0_usa
+Matching a param
+"""
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    from sys import argv
+    conn = sql.connect(host="localhost", user=argv[1],
+                       passwd=argv[2], db=argv[3], port=3306)
+
+    cursor = conn.cursor()
+    cursor.prepare("SELECT * FROM `states` WHERE name\
+                   LIKE  :p ORDER BY `states`.`id` ASC")
+
+    cursor.execute([argv[4]])
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    conn.close()
