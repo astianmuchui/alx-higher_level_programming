f99b2171072ee0b3d6804ae11bbdc17bd9197564
astianmuchui <astianmuchui@gmail.com>
2023-11-18 14:04:06 +0300

    escape sql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Module to list all states from the database hbtn_0e_0_usa
+Starting with N
+"""
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    from sys import argv
+    conn = sql.connect(host="localhost", user=argv[1],
+                       passwd=argv[2], db=argv[3], port=3306)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `states` WHERE name\
+                   LIKE %s ORDER BY `states`.`id` ASC", (argv[4],))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    conn.close()
