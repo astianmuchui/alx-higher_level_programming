3b78f992aeec00b05f61ad1e82228f6bf02aa09d
astianmuchui <astianmuchui@gmail.com>
2023-11-18 10:34:06 +0300

    get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Module to list all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+        import MySQLdb as sql
+        from sys import argv
+        import sqlalchemy as orm
+
+        conn = orm.connect(host="localhost", user=argv[2], passwd=argv[3], db=argv[4], port=3306)
+
+        if conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+                for row in cursor.fetchall():
+                        print(row)
