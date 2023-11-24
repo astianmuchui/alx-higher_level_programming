1832c69552cf11bdcdf5f7ac249a6b78470a6b32
astianmuchui <astianmuchui@gmail.com>
2023-11-24 14:12:50 +0300

    Define city model

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Module to define the City model
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+class City(Base):
+    """
+    City class inherits from Base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
