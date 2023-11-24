0c900e79377e688a2a39f895a6cd45ddb89e8fe7
astianmuchui <astianmuchui@gmail.com>
2023-11-24 10:56:11 +0300

    Create first orm

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.ext import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
