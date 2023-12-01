09d576305adc4d3f6a04721798fbba1a40559a75
astianmuchui <astianmuchui@gmail.com>
2023-12-01 03:57:04 +0300

    Send url params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a post request with params
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+url=$1 email="test@gmail.com" subject="I will always be here for PLD" response=$(curl -s -X POST -d "email=$email&subject=$subject" "$url" -o response.txt) && { echo "POST params:"; echo "    email: $email"; echo "    subject: $subject"; echo ""; echo "Response body:"; cat response.txt; } && rm -f response.txt
