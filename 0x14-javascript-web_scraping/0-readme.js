ba9c1dc5f48cfde0cbb83b53ab2525d553dc58e3
astianmuchui <astianmuchui@gmail.com>
2024-01-08 12:40:45 +0300

    Script to read the contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        });
+  if (err) throw err;
+  console.log(data);
+});
