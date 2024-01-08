f6b092ee0353f144e2e11990fc6a78995f3ceac1
astianmuchui <astianmuchui@gmail.com>
2024-01-08 07:53:00 +0300

    Error code body

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the
+body of the response (decoded in utf-8)
+"""
+
+if __name__ == "__main__":
+        from urllib import request, error
+        from sys import argv
+
+        try:
+                with request.urlopen(argv[1]) as response:
+                        print(response.read().decode("utf-8"))
+        except error.HTTPError as e:
+                print("Error code: {}".format(e.code))
