2e285693666f89a931e016967e0b5f93ccd7e543
astianmuchui <astianmuchui@gmail.com>
2024-01-08 08:03:45 +0300

    hbtn.io status

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header
+using the requests module
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    response = requests.get(argv[1])
+    print(response.headers.get("X-Request-Id"))
