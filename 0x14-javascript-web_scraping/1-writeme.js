81c9222d1fd345c77c1d462d9065a5d7813f1c10
astianmuchui <astianmuchui@gmail.com>
2024-01-08 12:43:42 +0300

    Script to write content to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.WriteStream(file, content, 'utf8', (err) => {
+  if (err) throw err;
+});
