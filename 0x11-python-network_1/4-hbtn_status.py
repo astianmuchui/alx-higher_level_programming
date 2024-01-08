b147b251e6e12a5a4996fc75dbb29176fb634716
astianmuchui <astianmuchui@gmail.com>
2024-01-08 08:04:22 +0300

    hbtn.io status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Fetches https://intranet.hbtn.io/status
+"""
+
+if __name__ == "__main__":
+    import requests
+
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
