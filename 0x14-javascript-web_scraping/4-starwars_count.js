95cab3269c7083231ecad82eafdf8f49746b1c71
astianmuchui <astianmuchui@gmail.com>
2024-01-08 13:21:10 +0300

    starwars api

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
